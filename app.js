15e9157378d9cf882bbce28e9d75e772f81c9153
SalahAssana <salahmassana@gmail.com>
2023-04-16 16:24:07 -0400

    Update HTML to include CSS and JS file

--- app.js
+++ app.js
@@ -1,73 +1,73 @@
-const listings = [
-  {
-    "title": "Featured Apps",
-    "apps": [
-      {
-          "name": "My Cool App",
-          "description": "A revolutionary new way to do whatever you want!",
-          "thumbImgUrl": "https://uxwing.com/wp-content/themes/uxwing/download/hand-gestures/good-icon.png",
-          "authorName": "Someone",
-          "linkUrl": "#"
-        },
-       {
-          "name": "My Cool App",
-          "description": "A revolutionary new way to do whatever you want!",
-          "thumbImgUrl": "https://uxwing.com/wp-content/themes/uxwing/download/hand-gestures/good-icon.png",
-          "authorName": "Someone",
-          "linkUrl": "#"
-        }
-    ]
-  },
- {
-    "title": "All Apps",
-     "apps": [
-       {
-          "name": "My Cool App",
-          "description": "A revolutionary new way to do whatever you want!",
-          "thumbImgUrl": "https://uxwing.com/wp-content/themes/uxwing/download/hand-gestures/good-icon.png",
-          "authorName": "Someone",
-          "linkUrl": "#"
-        },
-       {
-          "name": "My Cool App",
-          "description": "A revolutionary new way to do whatever you want!",
-          "thumbImgUrl": "https://uxwing.com/wp-content/themes/uxwing/download/hand-gestures/good-icon.png",
-          "authorName": "Someone",
-          "linkUrl": "#"
-        },
-       {
-          "name": "My Cool App",
-          "description": "A revolutionary new way to do whatever you want!",
-          "thumbImgUrl": "https://uxwing.com/wp-content/themes/uxwing/download/hand-gestures/good-icon.png",
-          "authorName": "Someone",
-          "linkUrl": "#"
-        },
-       {
-          "name": "My Cool App",
-          "description": "A revolutionary new way to do whatever you want!",
-          "thumbImgUrl": "https://uxwing.com/wp-content/themes/uxwing/download/hand-gestures/good-icon.png",
-          "authorName": "Someone",
-          "linkUrl": "#"
-        },
-       {
-          "name": "My Cool App",
-          "description": "A revolutionary new way to do whatever you want!",
-          "thumbImgUrl": "https://uxwing.com/wp-content/themes/uxwing/download/hand-gestures/good-icon.png",
-          "authorName": "Someone",
-          "linkUrl": "#"
-        },
-       {
-          "name": "My Cool App",
-          "description": "A revolutionary new way to do whatever you want!",
-          "thumbImgUrl": "https://uxwing.com/wp-content/themes/uxwing/download/hand-gestures/good-icon.png",
-          "authorName": "Someone",
-          "linkUrl": "#"
-        }
-     ]
-}
-]
+document.addEventListener('DOMContentLoaded', () => {
+  const listings = [
+    {
+      "title": "Featured Apps",
+      "apps": [
+        {
+            "name": "My Cool App",
+            "description": "A revolutionary new way to do whatever you want!",
+            "thumbImgUrl": "https://uxwing.com/wp-content/themes/uxwing/download/hand-gestures/good-icon.png",
+            "authorName": "Someone",
+            "linkUrl": "#"
+          },
+         {
+            "name": "My Cool App",
+            "description": "A revolutionary new way to do whatever you want!",
+            "thumbImgUrl": "https://uxwing.com/wp-content/themes/uxwing/download/hand-gestures/good-icon.png",
+            "authorName": "Someone",
+            "linkUrl": "#"
+          }
+      ]
+    },
+   {
+      "title": "All Apps",
+       "apps": [
+         {
+            "name": "My Cool App",
+            "description": "A revolutionary new way to do whatever you want!",
+            "thumbImgUrl": "https://uxwing.com/wp-content/themes/uxwing/download/hand-gestures/good-icon.png",
+            "authorName": "Someone",
+            "linkUrl": "#"
+          },
+         {
+            "name": "My Cool App",
+            "description": "A revolutionary new way to do whatever you want!",
+            "thumbImgUrl": "https://uxwing.com/wp-content/themes/uxwing/download/hand-gestures/good-icon.png",
+            "authorName": "Someone",
+            "linkUrl": "#"
+          },
+         {
+            "name": "My Cool App",
+            "description": "A revolutionary new way to do whatever you want!",
+            "thumbImgUrl": "https://uxwing.com/wp-content/themes/uxwing/download/hand-gestures/good-icon.png",
+            "authorName": "Someone",
+            "linkUrl": "#"
+          },
+         {
+            "name": "My Cool App",
+            "description": "A revolutionary new way to do whatever you want!",
+            "thumbImgUrl": "https://uxwing.com/wp-content/themes/uxwing/download/hand-gestures/good-icon.png",
+            "authorName": "Someone",
+            "linkUrl": "#"
+          },
+         {
+            "name": "My Cool App",
+            "description": "A revolutionary new way to do whatever you want!",
+            "thumbImgUrl": "https://uxwing.com/wp-content/themes/uxwing/download/hand-gestures/good-icon.png",
+            "authorName": "Someone",
+            "linkUrl": "#"
+          },
+         {
+            "name": "My Cool App",
+            "description": "A revolutionary new way to do whatever you want!",
+            "thumbImgUrl": "https://uxwing.com/wp-content/themes/uxwing/download/hand-gestures/good-icon.png",
+            "authorName": "Someone",
+            "linkUrl": "#"
+          }
+       ]
+  }];
 
-const { createApp } = Vue
+  const { createApp } = Vue;
 
   createApp({
     data() {
@@ -75,4 +75,5 @@ const { createApp } = Vue
         listings: listings
       }
     }
-  }).mount('#listings')
+  }).mount('#listings');
+});
